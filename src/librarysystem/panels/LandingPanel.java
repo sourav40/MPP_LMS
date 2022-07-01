d8ae365aa40ff54ee55d0fd55a471b4ee5174d23
sourav.shrestha <souravshrestha40@gmail.com>
2022-06-30 23:04:38 -0500

    dashboard image and count added.

--- src/librarysystem/panels/LandingPanel.java
+++ src/librarysystem/panels/LandingPanel.java
@@ -6,43 +6,70 @@ import librarysystem.LibrarySystem;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 public class LandingPanel implements MessageableWindow {
-    LibrarySystem librarySystem;
+	LibrarySystem librarySystem;
 
-    private final ControllerInterface sc = SystemController.INSTANCE;
+	private final ControllerInterface sc = SystemController.INSTANCE;
 
-    public void setLibrarySystem(LibrarySystem librarysystem) {
-        librarySystem = librarysystem;
-    }
+	public void setLibrarySystem(LibrarySystem librarysystem) {
+		librarySystem = librarysystem;
+	}
 
-    private JPanel mainPanel;
+	private JPanel mainPanel;
 
-    String pathToImage;
+	String pathToImage;
 
-    public JPanel getMainPanel() {
-        return mainPanel;
-    }
+	public JPanel getMainPanel() {
+		return mainPanel;
+	}
 
-    public LandingPanel() {
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(1, 1));
-        setPathToImage();
-        insertSplashImage();
-    }
+	public LandingPanel() {
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new FlowLayout());
+//		setPathToImage();
+//		insertSplashImage();
+		defineTopPanel();
+	}
 
-    private void setPathToImage() {
-        String currDirectory = System.getProperty("user.dir");
-        pathToImage = currDirectory + "/src/librarysystem/library.png";
-    }
+	private void setPathToImage() {
+		String currDirectory = System.getProperty("user.dir");
+		pathToImage = currDirectory + "/src/librarysystem/library.png";
+	}
 
-    private void insertSplashImage() {
-        ImageIcon image = new ImageIcon(pathToImage);
-        mainPanel.add(new JLabel(image));
-    }
+	private void insertSplashImage() {
+		ImageIcon image = new ImageIcon(pathToImage);
+		mainPanel.add(new JLabel(image));
+	}
 
-    @Override
-    public void updateData() {
+	private void defineTopPanel() {
 
-    }
+		AddBookCopyPanel abcp = new AddBookCopyPanel();
+		NewMemberPanel nmp = new NewMemberPanel();
+
+		String bookPath = System.getProperty("user.dir") + "/src/librarysystem/book.png";
+		Icon bookIcon = new ImageIcon(bookPath);
+		JButton loginLabel = new JButton("Available Books : " + sc.allBookIds().size(), bookIcon);
+		loginLabel.setBackground(Color.LIGHT_GRAY);
+		loginLabel.setOpaque(true);
+		loginLabel.setBorderPainted(false);
+		loginLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+
+		String memberPath = System.getProperty("user.dir") + "/src/librarysystem/member.png";
+		Icon memberIcon = new ImageIcon(memberPath);
+		JButton member = new JButton("Members : " + sc.allMemberIds().size(), memberIcon);
+		member.setBackground(Color.LIGHT_GRAY);
+		member.setOpaque(true);
+		member.setBorderPainted(false);
+		member.setFont(new Font("Arial", Font.PLAIN, 20));
+
+		mainPanel.add(loginLabel);
+		mainPanel.add(member);
+	}
+
+	@Override
+	public void updateData() {
+
+	}
 }
